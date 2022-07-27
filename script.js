d9710432f8c733575221a2c74f8ab13aa436f893
jhonatan <jhoncordeiro72@gmail.com>
2022-07-27 10:18:48 -0300

    cria pagina de contato

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ function cadastrar(event){
 
     const form = document.getElementById('form');
     const name = document.getElementById('name');
+    const age = document.getElementById('age');
+    const blood = document.getElementById('blood');
+    const cpf = document.getElementById('cpf');
+    const cellphone = document.getElementById('cellphone');
+    const email = document.getElementById('email');
+    const social_networks = document.getElementById('social_networks');
     const phone = document.getElementById('phone');
-    const nationality = document.getElementById('nationality');
+    const address = document.getElementById('address');
+    const house_number = document.getElementById('house_number');
+    const district = document.getElementById('district');
+    const complement = document.getElementById('complement');
     const city = document.getElementById('city');
+    const zip_code = document.getElementById('zip_code');
+    const state = document.getElementById('state');
+    const nationality = document.getElementById('nationality');
+
 
     const data = Object.fromEntries(new FormData(form).entries());
     console.log(data)
@@ -16,7 +29,20 @@ function cadastrar(event){
     result.appendChild(node)
     
     name.value=""
+    age.value=""
+    blood.value=""
+    cpf.value=""
+    cellphone.value=""
+    email.value=""
+    social_networks.value=""
     phone.value=""
-    nationality.value=""
+    address.value=""
+    house_number.value=""
+    district.value=""
+    complement.value=""
     city.value=""
+    zip_code.value=""
+    state.value=""
+    nationality.value=""
+    
 }
